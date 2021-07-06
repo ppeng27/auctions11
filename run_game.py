00c7a3d199befd4cfa42ead264b6a0801736291c
Phoebe Peng <p.peng27@outlook.com.au>
2021-07-06 15:00:22 +1000

    templates

---
+++ run_game.py
@@ -0,0 +1,20 @@
+from gameEngine import GameEngine, NPCRandomBot
+import importlib
+
+# List your bots here
+botsToRun = {
+    "examples.randomBidder":4,
+    "examples.randomAccuser":1,
+    "NPC": 4
+}
+
+engine = GameEngine()
+
+for b in botsToRun:
+    for i in range(botsToRun[b]):
+        if b=="NPC":
+            engine.registerBot(NPCRandomBot(),team="NPC")
+        else:
+            botClass = importlib.import_module(b)
+            engine.registerBot(botClass.CompetitorInstance(),team=b)
+engine.runGame()
