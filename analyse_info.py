b9a9deb438089e020e4dfd1aac99922753f73c20
tylergan <tgan2615@gmail.com>
2021-07-11 00:45:32 +1000

    Adding changes to analyser

--- analyse_info.py
+++ analyse_info.py
@@ -8,6 +8,10 @@ import time
 
 class Analyser:
     def __init__(self):
+        self.phase1_in = []
+        self.phase2_in = []
+        self.phase3_in = []
+
         self.phase1 = []
         self.phase2 = []
         self.phase3 = []
@@ -24,6 +28,14 @@ class Analyser:
 
     def std(self, data):
         return sqrt(self.variance(data))
+
+    def covariance(self, data_x, data_y):
+        mu_x, mu_y = self.mean(data_x), self.mean(data_y)
+        return sum(map(lambda x, y: (x - mu_x) * (y - mu_y), data_x, data_y))/(len(data_x) - 1)
+
+    def pearson_coe(self, data_x, data_y):
+        cov = self.covariance(data_x, data_y)
+        return cov/(self.std(data_x) * self.std(data_y))
 
     def skew(self, data):
         mu = self.mean(data)
@@ -50,45 +62,59 @@ class Analyser:
 
                 bidder = []
                 npc_prevs = [None for _ in range(len(npcs))]
+                npc_bids = npc_prevs.copy()
                 for i, bid in enumerate(bids):
                     if len(bidder) != (len(set(bidder))):
                         npc_prevs = [int(bids[i - 1].split(':')[-1]) if prev == None else prev for prev in npc_prevs]
-                        npc_bids = [1 if npc in set(bidder) else 0 for npc in npcs]
+                        npc_bids = [0 if bid is None else bid for bid in npc_bids]
 
                         for j, prev in enumerate(npc_prevs):
                             if prev > true_mu * 3/4:
+                                analyser.phase3_in.append(npc_prevs[j])
                                 analyser.phase3.append(npc_bids[j])
                             elif prev > true_mu/4:
+                                analyser.phase2_in.append(npc_prevs[j])
                                 analyser.phase2.append(npc_bids[j])
                             else:
+                                analyser.phase1_in.append(npc_prevs[j])
                                 analyser.phase1.append(npc_bids[j])
 
                         bidder = [bidder[-1]]
+                        npc_prevs = [None for _ in range(len(npcs))]
+                        npc_bids = npc_prevs.copy()
 
                     bot, amt = list(map(lambda x: int(x), bid.split(':')))
 
                     if bot in npcs:
                         npc_prevs[npcs.index(bot)] = int(bids[i - 1].split(':')[-1])
+                        npc_bids[npcs.index(bot)] = amt
 
                     bidder.append(bot)
 
     def calculate(self):
         self.x_coors = [1, 2, 3]
         self.freqs = [
-            self.phase1.count(1)/len(self.phase1),
-            self.phase2.count(1)/len(self.phase2),
-            self.phase3.count(1)/len(self.phase3)
+            (len(self.phase1) - self.phase1.count(0))/len(self.phase1),
+            (len(self.phase2) - self.phase2.count(0))/len(self.phase2),
+            (len(self.phase3) - self.phase3.count(0))/len(self.phase3)
         ]
         self.skews = [
             self.skew(self.phase1),
             self.skew(self.phase2),
             self.skew(self.phase3)
         ]
+        self.pear_coes = [
+            self.pearson_coe(self.phase1_in, self.phase1),
+            self.pearson_coe(self.phase2_in, self.phase2),
+            self.pearson_coe(self.phase3_in, self.phase3)
+        ]
 
     def show(self):
         self.calculate()
+
         m1, b1 = np.polyfit(np.array(self.x_coors), np.array(self.freqs), 1)
         m2, b2 = np.polyfit(np.array(self.x_coors), np.array(self.skews), 1)
+        m3, b3 = np.polyfit(np.array(self.x_coors), np.array(self.pear_coes), 1)
 
         plt.xticks(np.arange(min(self.x_coors), max(self.x_coors)+1, 1.0))
 
@@ -100,12 +126,17 @@ class Analyser:
         plt.scatter(2, self.skews[1], alpha=0.5, label='Phase 2 Skew | {}'.format(self.skews[1]))
         plt.scatter(3, self.skews[2], alpha=0.5, label='Phase 3 Skew | {}'.format(self.skews[2]))
 
+        plt.scatter(1, self.pear_coes[0], alpha=0.5, label='Phase 1 r | {}'.format(self.pear_coes[0]))
+        plt.scatter(2, self.pear_coes[1], alpha=0.5, label='Phase 2 r | {}'.format(self.pear_coes[1]))
+        plt.scatter(3, self.pear_coes[2], alpha=0.5, label='Phase 3 r | {}'.format(self.pear_coes[2]))
+
         plt.plot(np.array(self.x_coors), m1 * np.array(self.x_coors) + b1, alpha=0.5, label='Frequencies')
         plt.plot(np.array(self.x_coors), m2 * np.array(self.x_coors) + b2, alpha=0.5, label='Skews')
+        plt.plot(np.array(self.x_coors), m3 * np.array(self.x_coors) + b3, alpha=0.5, label='Coes')
 
         plt.title('NPC Bidding Behaviour per Phase')
         plt.xlabel('Phases')
-        plt.ylabel('% Density (Freq) | Skewness')
+        plt.ylabel('% Density (Freq) | Skewness (Skew) | Correlation Coe (r)')
         plt.legend(loc='upper left')
         plt.show()
 
